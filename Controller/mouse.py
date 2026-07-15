ae97ee6a68e506549d8e463a968eaba61b13c176
Júlio Monteiro <julioalvedmonteiro@gmail.com>
2026-07-15 02:05:50 -0300

    Adicionando  a função no mouse de  apagar, mover para todos os lados e mover a figura enquanto o botão esquerdo do mouse é pressionado

--- Controller/mouse.py
+++ Controller/mouse.py
@@ -1,3 +1,4 @@
+import copy
 from View.opcoes import *
 from View.tela import *
 from View.cores import *
@@ -25,7 +26,11 @@ class Mouse:
             self.canvas = canvas
             self.desenhador = desenhador
             self.tipo_figura = tipo_figura_var
-
+            self.figura_selecionada = None
+            self.arrastando = False
+            self.ultimo_x = 0
+            self.ultimo_y = 0
+            self.buffer = None
 
             #criacao das instancias de cada ferramenta
             self.ferramentas = {
@@ -48,21 +53,43 @@ class Mouse:
             canvas.bind("<ButtonPress-1>", self.clique_no_mouse)
             canvas.bind("<B1-Motion>", self.mover_mouse)
             canvas.bind("<ButtonRelease-1>", self.soltar_mouse)
+            canvas.bind_all("<Delete>", self.apagar_selecionada)
+            canvas.bind_all("<Right>", self.mover_para_frente)
+            canvas.bind_all("<Left>", self.mover_para_tras)
+            canvas.bind_all("<Up>", self.mover_para_topo)
+            canvas.bind_all("<Down>", self.mover_para_fundo)
+            canvas.bind_all("<Control-c>", self.copiar_selecionada)
+            canvas.bind_all("<Control-v>", self.colar_selecionada)
 
       # Ao mudar o option menu que escolhe a ferramenta de desenho essa funcao muda a ferramenta escolhida
       def muda_ferramenta(self, *args):
             nome_ferramenta = self.tipo_figura.get()
-            self.ferramenta_desenho = self.ferramentas.get(nome_ferramenta, self.ferramentas["Linha"])
+            if nome_ferramenta == "Seleção":
+                  self.ferramenta_desenho = None
+            else:
+                  self.ferramenta_desenho = self.ferramentas.get(nome_ferramenta, self.ferramentas["Linha"])
       
 
 
-
       def clique_no_mouse(self, event):
+         self.canvas.focus_set()
+         if self.tipo_figura.get() == "Seleção":
+               # Seleciona a figura clicada para executar as operações de edição
+               self.selecionar_figura(event)
+               return
 
          self.ferramenta_desenho.mouse_pressionado(event)
 
 
       def mover_mouse(self, event):
+         if self.tipo_figura.get() == "Seleção":
+               # Move a figura selecionada enquanto o botão esquerdo do mouse é arrastado
+               if self.arrastando and self.figura_selecionada is not None:
+                     self.mover_figura_selecionada(event.x - self.ultimo_x, event.y - self.ultimo_y)
+                     self.ultimo_x = event.x
+                     self.ultimo_y = event.y
+               return
+
          self.ferramenta_desenho.mouse_arrastado(event)
 
 
